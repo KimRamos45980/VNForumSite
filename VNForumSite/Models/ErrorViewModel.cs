f117d4d56f86d129a08dab0d4b96011c6ceb7077
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-10-02 10:09:51 -0700

    Add project files.

---
+++ VNForumSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VNForumSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
