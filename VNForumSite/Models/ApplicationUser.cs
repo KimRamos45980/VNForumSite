4019e0cba31c41ca0e31bca51c9097e89bbf312d
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-10-06 11:45:58 -0700

    Created ApplicationUser Class

---
+++ VNForumSite/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace VNForumSite.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string CustomTag { get; set; }
+    }
+}
