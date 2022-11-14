7bca14bd21d3040bcff9eb6b599d8609df00a4ce
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-11-14 12:23:34 -0800

    Created User Roles

---
+++ VNForumSite/Data/ContextSeed.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using VNForumSite.Models;
+
+namespace VNForumSite.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            // Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(UserRoles.User.ToString()));
+        }
+    }
+}
