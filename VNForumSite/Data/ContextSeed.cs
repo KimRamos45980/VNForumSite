89e7b57c678d29b07afd0978da530d19ab4e974f
Kimberly Ramos <kimberlyramos45980@yahoo.com>
2022-11-14 12:38:49 -0800

    Created Admin account

--- VNForumSite/Data/ContextSeed.cs
+++ VNForumSite/Data/ContextSeed.cs
@@ -12,5 +12,24 @@ namespace VNForumSite.Data
             await roleManager.CreateAsync(new IdentityRole(UserRoles.Admin.ToString()));
             await roleManager.CreateAsync(new IdentityRole(UserRoles.User.ToString()));
         }
+
+        public static async Task SeedAdminAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "admin",
+                Email = "admin@mail.com"
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if(user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$$word");
+                    await userManager.AddToRoleAsync(defaultUser, UserRoles.Admin.ToString());
+                    await userManager.AddToRoleAsync(defaultUser, UserRoles.User.ToString());
+                }
+            }
+        }
     }
 }
